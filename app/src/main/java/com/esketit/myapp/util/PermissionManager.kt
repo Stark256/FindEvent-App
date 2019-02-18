9a85dd6b2ec4367575c498f009279fbe04996a1c
Stark256 <starkhyperpro@gmail.com>
2019-02-18 08:27:10 +0200

    Permission manager Circle image view

---
+++ app/src/main/java/com/esketit/myapp/util/PermissionManager.kt
@@ -0,0 +1,173 @@
+package com.esketit.myapp.util
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import com.esketit.myapp.App
+import com.esketit.myapp.R
+import com.esketit.myapp.view.BaseDialog
+
+import timber.log.Timber
+
+class PermissionManager {
+
+    private var dialog: BaseDialog? = null
+
+//    fun isPermissionLocationGranted(context: Context = App.instance): Boolean{
+//        return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+//    }
+
+    fun isPermissionLocationGranted(activity: AppCompatActivity, requestPermission: (() -> Unit)? = null): Boolean{
+        return permissionLocationGranted(activity, requestPermission).isSuccess
+    }
+
+    fun isPermissionCameraWriteStorageGranted(activity: AppCompatActivity, request: (() ->  Unit)? = null): Boolean {
+        return permissionCameraGranted(activity, request).isSuccess && permissionStorageForCameraGranted(activity, request).isSuccess
+    }
+
+    private fun permissionStorageForCameraGranted(activity: AppCompatActivity, request: (() ->  Unit)? = null): PermissionResponse {
+        return checkPermission(
+            activity.getString(R.string.reject_permission),
+            activity,
+            request,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            activity.getString(R.string.required_write_to_storage_permission),
+            PERMISSION_STORAGE_FOR_CAMERA)
+    }
+
+    private fun permissionCameraGranted(activity: AppCompatActivity, request: (() -> Unit)? = null): PermissionResponse {
+        return checkPermission(
+            activity.getString(R.string.reject_permission),
+            activity,
+            request,
+            Manifest.permission.CAMERA,
+            activity.getString(R.string.required_camera_permission),
+            PERMISSION_CAMERA)
+    }
+
+    fun permissionLocationGranted(activity: AppCompatActivity,  requestPermission: (() -> Unit)? = null): PermissionResponse {
+        return checkPermission(
+            activity.getString(R.string.reject_permission),
+            activity,
+            requestPermission,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            activity.getString(R.string.required_location_permission),
+            PERMISSION_ACCESS_FINE_LOCATION
+        )
+    }
+
+    fun isPermissionForGalleryGranted(activity: AppCompatActivity, request: (() ->  Unit)? = null): Boolean {
+        return permissionStorageForGalleryGranted(activity, request).isSuccess
+    }
+
+    fun permissionStorageForGalleryGranted(activity: AppCompatActivity, requestPermission: (() -> Unit)? = null): PermissionResponse {
+        return checkPermission(
+            activity.getString(R.string.reject_permission),
+            activity,
+            requestPermission,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            activity.getString(R.string.required_write_to_storage_permission),
+            PERMISSION_STORAGE_FOR_GALLERY)
+    }
+
+
+    private fun checkPermission(checkText: String, activity: AppCompatActivity, requestPermission: (() -> Unit)? = null,
+                                permission: String, infoMessage: String, requestCode: Int): PermissionResponse {
+        if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                if (shouldShowRequestPermission(permission)) {
+                    dialog?.dismiss()
+
+                    showInfoDialog(
+                        checkText,
+                        activity,
+                        requestPermission,
+                        infoMessage,
+                        arrayOf(permission),
+                        requestCode)
+                }
+            } else {
+                if(requestPermission != null){
+                    requestPermission()
+                }else{
+                    requestPermissions(
+                        activity,
+                        arrayOf(permission),
+                        requestCode)
+                }
+
+            }
+            return PermissionResponse(false, false)
+        }
+        return PermissionResponse(true, false)
+    }
+
+    private fun shouldShowRequestPermission(permission: String): Boolean {
+        return when (permission) {
+            Manifest.permission.ACCESS_FINE_LOCATION -> false//!shouldHideRequestPermissionLocation()
+            else -> true
+        }
+    }
+
+//    private fun shouldHideRequestPermissionLocation(): Boolean {
+//        return SettingsPrefs().showPermissionLocationDialog
+//    }
+
+    private fun requestPermissions(activity: Activity, permissions: Array<String>, requestCode: Int, requestPermission: (() -> Unit)? = null) {
+        if(requestPermission != null){
+            requestPermission()
+        }else{
+            ActivityCompat.requestPermissions(activity, permissions, requestCode)
+        }
+    }
+
+    private fun showInfoDialog(checkText: String, activity: AppCompatActivity, requestPermission: (() -> Unit)? = null, message: String,
+                               permissions: Array<String>, requestCode: Int) {
+        dialog = BaseDialog.Builder()
+            .setTitle(activity.getString(R.string.permission_required))
+            .setMessage(message)
+            .setPositiveButton(R.string.ok) {view ->
+                requestPermissions(activity, permissions, requestCode, requestPermission)
+                dialog?.dismiss()
+                dialog = null
+
+            }
+            .setNegativeButton(R.string.cancel) {view ->
+                dialog?.dismiss()
+                dialog = null
+            }
+            //.setOnCheckListener(checkText) { view, isChecked -> disablePermissionDialog(permissions, isChecked) }
+            .build()
+        dialog?.show(activity.supportFragmentManager, null)
+    }
+
+//    private fun disablePermissionDialog(permissions: Array<String>, isChecked: Boolean) {
+//        Timber.d("Is checked $isChecked")
+//        for (permission in permissions) {
+//            when (permission) {
+//                Manifest.permission.ACCESS_FINE_LOCATION ->
+//                    //SettingsPrefs().showPermissionLocationDialog = isChecked
+//                else -> {
+//                }
+//            }
+//        }
+//    }
+
+    data class PermissionResponse(val isSuccess: Boolean, val hideDialog: Boolean)
+
+    companion object {
+        @JvmField
+        val PERMISSION_ACCESS_FINE_LOCATION = 101
+        @JvmField
+        val PERMISSION_STORAGE_FOR_GALLERY = 102
+        @JvmField
+        val PERMISSION_CAMERA = 103
+        @JvmField
+        val PERMISSION_STORAGE_FOR_CAMERA = 104
+    }
+
+}
---
+++ app/src/main/java/com/esketit/myapp/view/BaseDialog.kt
@@ -0,0 +1,151 @@
+package com.esketit.myapp.view
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.support.annotation.LayoutRes
+import android.support.annotation.StringRes
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import com.esketit.myapp.App
+import com.esketit.myapp.R
+
+
+open class BaseDialog: DialogFragment() {
+
+    var positiveBtn: Button? = null
+    var negativeBtn: Button? = null
+    var titleText: TextView? = null
+    var messageText: TextView? = null
+    var checkTextView: TextView? = null
+    var checkBox: CheckBox? = null
+
+    var title: String? = null
+    var message: String? = null
+    var positiveText: String? = null
+    var negativeText: String? = null
+    var checkText: String? = null
+    var negativeAction: ((View) -> Unit)? = null
+    var positiveAction: ((View) -> Unit)? = null
+    var cancelAction: ((View) -> Unit)? = null
+    var checkAction: ((View, Boolean) -> Unit)?= null
+
+    @LayoutRes
+    private var resource: Int = R.layout.dialog_base
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val view = inflater.inflate(getLayoutResource(), container, false)
+        initView(view)
+
+        return view
+    }
+
+    fun getLayoutResource(): Int{
+        return resource
+    }
+
+    class Builder {
+        private val baseDialog: BaseDialog =
+            BaseDialog()
+
+        fun setOnCheckListener(checkText: String, checkAction: ((View, Boolean) -> Unit)?): Builder {
+            baseDialog.checkText = checkText
+            baseDialog.checkAction = checkAction
+            return this
+        }
+
+        fun setPositiveButton(@StringRes textId: Int, positiveAction: ((View) -> Unit)?): Builder {
+            return setPositiveButton(App.instance.getString(textId), positiveAction)
+        }
+
+        fun setPositiveButton(text: String, positiveAction: ((View) -> Unit)?): Builder {
+            baseDialog.positiveText = text
+            baseDialog.positiveAction = positiveAction
+            return this
+        }
+
+        fun setNegativeButton(@StringRes textId: Int, negativeAction: ((View) -> Unit)?): Builder {
+            return setNegativeButton(App.instance.getString(textId), negativeAction)
+        }
+
+        fun setNegativeButton(text: String, negativeAction: ((View) -> Unit)?): Builder {
+            baseDialog.negativeText = text
+            baseDialog.negativeAction = negativeAction
+            return this
+        }
+
+        fun setOnCancelListener(cancelAction: ((View) -> Unit)?): Builder {
+            baseDialog.cancelAction = cancelAction
+            return this
+        }
+
+        fun setMessage(text: String): Builder {
+            baseDialog.message = text
+            return this
+        }
+
+        fun setMessage(@StringRes textId: Int): Builder {
+            return setMessage(App.instance.getString(textId))
+        }
+
+        fun setTitle(title: String): Builder {
+            baseDialog.title = title
+            return this
+        }
+
+        fun setTitle(@StringRes textId: Int): Builder {
+            return setTitle(App.instance.getString(textId))
+        }
+
+        fun build(): BaseDialog {
+            return baseDialog
+        }
+    }
+
+    protected fun initView(view: View) {
+        positiveBtn = view.findViewById(R.id.positiveButton)
+        negativeBtn = view.findViewById(R.id.negativeButton)
+        titleText = view.findViewById(R.id.titleTextView)
+        messageText = view.findViewById(R.id.messageTextView)
+        checkTextView = view.findViewById(R.id.checkboxTextView)
+        checkBox = view.findViewById(R.id.checkbox)
+
+        if (positiveAction == null) {
+            positiveBtn?.visibility = View.INVISIBLE
+        } else {
+            positiveBtn?.text = positiveText
+            positiveBtn?.setOnClickListener { positiveAction?.invoke(view) }
+        }
+
+
+        if (negativeAction == null) {
+            negativeBtn?.visibility = View.INVISIBLE
+        } else {
+            negativeBtn?.text = negativeText
+            negativeBtn?.setOnClickListener { negativeAction?.invoke(view) }
+
+            titleText?.text = title
+            messageText?.text = message
+
+        }
+
+        if(checkAction == null){
+            checkBox?.visibility = View.GONE
+            checkTextView?.visibility = View.GONE
+        }else{
+            checkTextView?.text = checkText
+            checkBox?.setOnCheckedChangeListener { _view, isChecked -> checkAction?.invoke(_view, isChecked) }
+        }
+    }
+
+}
