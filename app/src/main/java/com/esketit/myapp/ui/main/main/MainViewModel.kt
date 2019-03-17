ba19a263d557617bc8bc5235cb608f6beb7f995e
Stark256 <starkhyperpro@gmail.com>
2019-03-17 19:26:16 +0200

    user active time. timer for updating time avery 1 minute.

---
+++ app/src/main/java/com/esketit/myapp/ui/main/main/MainViewModel.kt
@@ -0,0 +1,20 @@
+package com.esketit.myapp.ui.main.main
+
+import android.arch.lifecycle.ViewModel
+import com.esketit.myapp.managers.Injector
+import java.util.*
+
+class MainViewModel: ViewModel(){
+
+    fun setTimer(){
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                Injector.userManager.updateActiveTime {
+                        firebaseResponse ->
+                }
+            }
+        }, 10000, 60000)
+    }
+
+}
